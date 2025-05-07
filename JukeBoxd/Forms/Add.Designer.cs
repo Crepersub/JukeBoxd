72eb3be6d10c3d82ec94fc8558f6d7ac2a8e6bba
iMyr <89942991+GgEzModded@users.noreply.github.com>
2025-05-07 23:38:19 +0300

    sync

--- JukeBoxd/Forms/Add.Designer.cs
+++ JukeBoxd/Forms/Add.Designer.cs
@@ -46,7 +46,6 @@
             ReviewLabel = new Label();
             ReviewTextBox = new TextBox();
             EntryDateTimePicker = new DateTimePicker();
-            panel1 = new Panel();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
@@ -57,7 +56,6 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
-            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // SongComboBox
@@ -101,7 +99,7 @@
             label2.AutoSize = true;
             label2.BackColor = Color.Transparent;
             label2.Font = new Font("Microsoft PhagsPa", 12F);
-            label2.Location = new Point(6, 5);
+            label2.Location = new Point(50, 113);
             label2.Name = "label2";
             label2.Size = new Size(69, 26);
             label2.TabIndex = 3;
@@ -111,7 +109,7 @@
             // 
             pictureBox1.BackColor = Color.Transparent;
             pictureBox1.Image = Properties.Resources.newEStar2;
-            pictureBox1.Location = new Point(10, 50);
+            pictureBox1.Location = new Point(58, 151);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(33, 62);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -122,7 +120,7 @@
             // 
             pictureBox2.BackColor = Color.Transparent;
             pictureBox2.Image = Properties.Resources.newEStar1;
-            pictureBox2.Location = new Point(42, 50);
+            pictureBox2.Location = new Point(90, 151);
             pictureBox2.Name = "pictureBox2";
             pictureBox2.Size = new Size(33, 62);
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -133,7 +131,7 @@
             // 
             pictureBox3.BackColor = Color.Transparent;
             pictureBox3.Image = Properties.Resources.newEStar2;
-            pictureBox3.Location = new Point(81, 50);
+            pictureBox3.Location = new Point(129, 151);
             pictureBox3.Name = "pictureBox3";
             pictureBox3.Size = new Size(33, 62);
             pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -144,7 +142,7 @@
             // 
             pictureBox4.BackColor = Color.Transparent;
             pictureBox4.Image = Properties.Resources.newEStar1;
-            pictureBox4.Location = new Point(113, 50);
+            pictureBox4.Location = new Point(161, 151);
             pictureBox4.Name = "pictureBox4";
             pictureBox4.Size = new Size(33, 62);
             pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -155,7 +153,7 @@
             // 
             pictureBox9.BackColor = Color.Transparent;
             pictureBox9.Image = Properties.Resources.newEStar2;
-            pictureBox9.Location = new Point(294, 50);
+            pictureBox9.Location = new Point(342, 151);
             pictureBox9.Name = "pictureBox9";
             pictureBox9.Size = new Size(33, 62);
             pictureBox9.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -166,7 +164,7 @@
             // 
             pictureBox10.BackColor = Color.Transparent;
             pictureBox10.Image = Properties.Resources.newEStar1;
-            pictureBox10.Location = new Point(326, 50);
+            pictureBox10.Location = new Point(374, 151);
             pictureBox10.Name = "pictureBox10";
             pictureBox10.Size = new Size(33, 62);
             pictureBox10.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -177,7 +175,7 @@
             // 
             pictureBox5.BackColor = Color.Transparent;
             pictureBox5.Image = Properties.Resources.newEStar2;
-            pictureBox5.Location = new Point(152, 50);
+            pictureBox5.Location = new Point(200, 151);
             pictureBox5.Name = "pictureBox5";
             pictureBox5.Size = new Size(33, 62);
             pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -188,7 +186,7 @@
             // 
             pictureBox6.BackColor = Color.Transparent;
             pictureBox6.Image = Properties.Resources.newEStar1;
-            pictureBox6.Location = new Point(184, 50);
+            pictureBox6.Location = new Point(232, 151);
             pictureBox6.Name = "pictureBox6";
             pictureBox6.Size = new Size(33, 62);
             pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -199,7 +197,7 @@
             // 
             pictureBox7.BackColor = Color.Transparent;
             pictureBox7.Image = Properties.Resources.newEStar2;
-            pictureBox7.Location = new Point(223, 50);
+            pictureBox7.Location = new Point(271, 151);
             pictureBox7.Name = "pictureBox7";
             pictureBox7.Size = new Size(33, 62);
             pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -210,7 +208,7 @@
             // 
             pictureBox8.BackColor = Color.Transparent;
             pictureBox8.Image = Properties.Resources.newEStar1;
-            pictureBox8.Location = new Point(255, 50);
+            pictureBox8.Location = new Point(303, 151);
             pictureBox8.Name = "pictureBox8";
             pictureBox8.Size = new Size(33, 62);
             pictureBox8.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -252,25 +250,6 @@
             EntryDateTimePicker.Size = new Size(615, 27);
             EntryDateTimePicker.TabIndex = 32;
             // 
-            // panel1
-            // 
-            panel1.BackColor = Color.Transparent;
-            panel1.Controls.Add(pictureBox6);
-            panel1.Controls.Add(label2);
-            panel1.Controls.Add(pictureBox8);
-            panel1.Controls.Add(pictureBox7);
-            panel1.Controls.Add(pictureBox5);
-            panel1.Controls.Add(pictureBox1);
-            panel1.Controls.Add(pictureBox10);
-            panel1.Controls.Add(pictureBox2);
-            panel1.Controls.Add(pictureBox9);
-            panel1.Controls.Add(pictureBox3);
-            panel1.Controls.Add(pictureBox4);
-            panel1.Location = new Point(53, 99);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(368, 130);
-            panel1.TabIndex = 34;
-            // 
             // Add
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
@@ -278,14 +257,24 @@
             BackgroundImage = Properties.Resources.background4;
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(743, 530);
-            Controls.Add(panel1);
+            Controls.Add(label2);
+            Controls.Add(pictureBox6);
             Controls.Add(EntryDateTimePicker);
+            Controls.Add(pictureBox8);
             Controls.Add(ReviewTextBox);
+            Controls.Add(pictureBox7);
             Controls.Add(ReviewLabel);
+            Controls.Add(pictureBox5);
             Controls.Add(label3);
+            Controls.Add(pictureBox1);
             Controls.Add(AddButton);
+            Controls.Add(pictureBox10);
+            Controls.Add(pictureBox2);
             Controls.Add(label1);
+            Controls.Add(pictureBox9);
             Controls.Add(SongComboBox);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox4);
             MaximizeBox = false;
             Name = "Add";
             StartPosition = FormStartPosition.CenterScreen;
@@ -300,8 +289,6 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -326,6 +313,5 @@
         private Label ReviewLabel;
         private TextBox ReviewTextBox;
         private DateTimePicker EntryDateTimePicker;
-        private Panel panel1;
     }
 }
